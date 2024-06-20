bf63384dd665c5e2fa5d1272c72c2d0e8ff58483
bala manohar <balamanohar.yeminedi@gmail.com>
2024-06-20 17:11:57 +0530

    hello component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
       <button>click</button>
+      <Hello />
     </div>
   );
 }
